02d4b25502b6dad09134989a3c817ff4c2cfe173
Ju99ernaut <brendonzee@gmail.com>
2020-11-30 15:49:53 +0200

    Update i18n

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import App from './App.vue';
 import store from './store';
 import router from './router';
 
-import '@/assets/css/tailwind.css';
+import i18n from "./i18n"
+import './registerServiceWorker'
 
+import '@/assets/css/tailwind.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
     render: h => h(App),
     router,
+    i18n,
     store
 }).$mount('#app');
