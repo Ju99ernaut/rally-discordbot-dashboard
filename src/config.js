09487eb6655a4163815a52e5382f94b9705d0a74
Ju99ernaut <brendonzee@gmail.com>
2020-12-01 17:18:48 +0200

    Update default gradient

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ export default {
         info: "#1d8cf8",
         danger: "#fd5d93",
         teal: "#00d6b4",
-        primaryGradient: ['rgba(76, 211, 150, 0.1)', 'rgba(53, 183, 125, 0)', 'rgba(119,52,169,0)'],
+        primaryGradient: ['rgba(66,134,121,0.15)', 'rgba(66,134,121,0.0)', 'rgba(66,134,121,0)'],
     }
 }
