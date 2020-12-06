8459a0cce37354d2a5bcdacd05132b4c54c9aa60
Ju99ernaut <brendonzee@gmail.com>
2020-12-06 14:18:04 +0200

    Placeholder component

---
+++ src/components/Placeholder.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-wrap -mx-3">
+    <div class="w-full mt-8 px-3">
+      <div
+        class="w-full bg-white rounded-lg shadow-md dark:bg-gray-800 p-4 mb-8 xl:mb-0"
+      >
+        <img src="@/assets/blank_canvas.svg" class="placeholder" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.placeholder {
+  height: calc(100vh - 250px);
+  margin: auto;
+}
+</style>
+
+<script>
+export default {
+  name: "placeholder",
+};
+</script>
