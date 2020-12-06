e000f4fcfa743921e727328d471a12f5768d47c1
Ju99ernaut <brendonzee@gmail.com>
2020-12-06 14:19:51 +0200

    Add page routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,11 @@
 import Dashboard from '@/components/Dashboard';
 import DashboardHome from '@/pages/Home';
+import Commands from '@/pages/Commands';
+import Controls from '@/pages/Controls';
+import FeatureRequest from '@/pages/FeatureRequest';
+import Logs from '@/pages/Logs';
+import Settings from '@/pages/Settings';
+import Setup from '@/pages/Setup';
 import NotFound from '@/pages/NotFound';
 
 export default [
@@ -9,7 +15,13 @@ export default [
         component: Dashboard,
         children: [
             { path: '/', redirect: { name: 'DashboardHome' } },
-            { path: 'home', name: 'DashboardHome', component: DashboardHome }
+            { path: 'home', name: 'DashboardHome', component: DashboardHome },
+            { path: 'commands', name: 'Commands', component: Commands },
+            { path: 'controls', name: 'Controls', component: Controls },
+            { path: 'features', name: 'FeatureRequest', component: FeatureRequest },
+            { path: 'logs', name: 'Logs', component: Logs },
+            { path: 'settings', name: 'Settings', component: Settings },
+            { path: 'setup', name: 'Setup', component: Setup },
         ]
     },
     { path: "*", component: NotFound },
