adc841c032137ddaf7735be0bb0ffe09f9d9af26
Ju99ernaut <brendonzee@gmail.com>
2020-12-07 14:51:54 +0200

    Add template for channels component

---
+++ src/components/Channels.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="w-12 shadow-lg h-screen bg-gray-100 dark:bg-gray-700 z-40"
+    id="channels-nav"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: "Channels",
+};
+</script>
