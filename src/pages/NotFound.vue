636d7a24c3582c018128bfb9315293493a86b6d2
Ju99ernaut <brendonzee@gmail.com>
2020-12-09 08:06:48 +0200

    Update theme, router link and illustration

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -9,7 +9,7 @@
         >
           404
         </div>
-        <div class="w-16 h-1 bg-blue-500 my-3 md:my-6"></div>
+        <div class="w-16 h-1 bg-red-500 my-3 md:my-6"></div>
         <p
           class="dark:text-gray-300 text-grey-darker text-2xl md:text-3xl font-light mb-8 leading-normal"
         >
@@ -17,7 +17,7 @@
         </p>
         <router-link
           class="bg-transparent dark:text-gray-300 text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg"
-          :to="{ path: 'home' }"
+          :to="{ path: '/' }"
           tag="button"
           >Go Home</router-link
         >
@@ -26,7 +26,7 @@
     <div
       class="relative pb-full md:flex p-8 md:pb-0 md:min-h-screen w-full md:w-1/2"
     >
-      <img src="@/assets/page_not_found.svg" />
+      <img src="@/assets/page_not_found_red.svg" />
     </div>
   </div>
 </template>
