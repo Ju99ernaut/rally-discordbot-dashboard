7db072b1b36483c90ada65d75f90fc9307060f6f
Ju99ernaut <brendonzee@gmail.com>
2020-12-12 15:18:52 +0200

    Template for editing channel and role mappings

--- src/pages/Setup.vue
+++ src/pages/Setup.vue
@@ -49,6 +49,221 @@
       </div>
       <span>Let's go &RightArrow;</span>
     </a>
+
+    <div class="flex flex-wrap -mx-3">
+      <div class="w-full xl:w-1/2 px-3">
+        <div
+          class="w-full bg-white rounded-lg shadow-md dark:bg-gray-800 p-4 mb-8 xl:mb-0"
+        >
+          <p class="text-xl mb-4">Channel Mappings</p>
+          <table class="w-full whitespace-no-wrap">
+            <thead>
+              <tr
+                class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
+              >
+                <th class="px-4 py-3">Coin</th>
+                <th class="px-4 py-3">Amount</th>
+                <th class="px-4 py-3">Channel</th>
+                <th class="px-4 py-3">Actions</th>
+              </tr>
+            </thead>
+            <tbody
+              class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
+            >
+              <tr
+                v-for="(channelMap, index) in channelMappings"
+                :key="index"
+                class="text-gray-700 dark:text-gray-400"
+              >
+                <td class="px-4 py-3">{{ channelMap.coin }}</td>
+                <td class="px-4 py-3">{{ channelMap.amount }}</td>
+                <td class="px-4 py-3">{{ channelMap.channel }}</td>
+                <td class="px-4 py-3">
+                  <div class="flex items-center space-x-4 text-sm">
+                    <button
+                      class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                      aria-label="Edit"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+                        ></path>
+                      </svg>
+                    </button>
+                    <button
+                      class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                      aria-label="Delete"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="px-3 py-3">
+                  <label class="block w-full text-sm">
+                    <input
+                      disabled
+                      class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                      placeholder="COIN"
+                    />
+                  </label>
+                </td>
+                <td class="px-3 py-3">
+                  <label class="block w-full text-sm">
+                    <input
+                      disabled
+                      class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                      placeholder="100"
+                    />
+                  </label>
+                </td>
+                <td class="px-3 py-3">
+                  <label class="block w-full text-sm">
+                    <input
+                      disabled
+                      class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                      placeholder="Channel1"
+                    />
+                  </label>
+                </td>
+                <td class="px-4 py-3">
+                  <button
+                    type="button"
+                    class="w-full inline-flex justify-center rounded-md border border-gray-500 shadow-sm px-4 py-2 text-base font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto sm:text-sm"
+                  >
+                    Add
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div class="w-full xl:w-1/2 px-3">
+        <div
+          class="w-full bg-white rounded-lg shadow-md dark:bg-gray-800 p-4 mb-8 xl:mb-0"
+        >
+          <p class="text-xl mb-4">Role Mappings</p>
+          <table class="w-full whitespace-no-wrap">
+            <thead>
+              <tr
+                class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
+              >
+                <th class="px-4 py-3">Coin</th>
+                <th class="px-4 py-3">Amount</th>
+                <th class="px-4 py-3">Role</th>
+                <th class="px-4 py-3">Actions</th>
+              </tr>
+            </thead>
+            <tbody
+              class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
+            >
+              <tr
+                v-for="(roleMap, index) in roleMappings"
+                :key="index"
+                class="text-gray-700 dark:text-gray-400"
+              >
+                <td class="px-4 py-3">{{ roleMap.coin }}</td>
+                <td class="px-4 py-3">{{ roleMap.amount }}</td>
+                <td class="px-4 py-3">{{ roleMap.role }}</td>
+                <td class="px-4 py-3">
+                  <div class="flex items-center space-x-4 text-sm">
+                    <button
+                      class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                      aria-label="Edit"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+                        ></path>
+                      </svg>
+                    </button>
+                    <button
+                      class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                      aria-label="Delete"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="px-3 py-3">
+                  <label class="block w-full text-sm">
+                    <input
+                      disabled
+                      class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                      placeholder="COIN"
+                    />
+                  </label>
+                </td>
+                <td class="px-3 py-3">
+                  <label class="block w-full text-sm">
+                    <input
+                      disabled
+                      class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                      placeholder="100"
+                    />
+                  </label>
+                </td>
+                <td class="px-3 py-3">
+                  <label class="block w-full text-sm">
+                    <input
+                      disabled
+                      class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                      placeholder="Role1"
+                    />
+                  </label>
+                </td>
+                <td class="px-4 py-3">
+                  <button
+                    disabled
+                    type="button"
+                    class="w-full inline-flex justify-center rounded-md border border-gray-500 shadow-sm px-4 py-2 text-base font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto sm:text-sm"
+                  >
+                    Add
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -63,6 +278,14 @@ export default {
   },
   computed: {
     ...mapState(["currentGuildId"]),
+  },
+  data() {
+    return {
+      channelMappings: [
+        { coin: "CHEWS", amount: "100", channel: "Chewy hideout" },
+      ],
+      roleMappings: [{ coin: "CHEWS", amount: "250", role: "VVVIP" }],
+    };
   },
 };
 </script>
