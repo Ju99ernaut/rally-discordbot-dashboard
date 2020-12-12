0d8f24758850161a547a733659320dea6418d69b
Ju99ernaut <brendonzee@gmail.com>
2020-12-12 13:43:48 +0200

    Open links in new page

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -74,6 +74,7 @@
               <a
                 class="flex"
                 href="https://discord.com/api/oauth2/authorize?client_id=769334375023640578&permissions=268438560&scope=bot"
+                target="_blank"
               >
                 <svg
                   class="w-5 h-5 mr-2"
