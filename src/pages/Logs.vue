bbfc97daaa5e94870a9a80321a4c06d12a53ba18
Ju99ernaut <brendonzee@gmail.com>
2020-12-20 17:56:42 +0200

    Add refresh button to logs

--- src/pages/Logs.vue
+++ src/pages/Logs.vue
@@ -2,7 +2,16 @@
   <div id="logs">
     <breadcrumbs :name="$t('sidebar.logs')" />
 
-    <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+    <button
+      @click="refresh"
+      class="bg-red-500 hover:bg-red-600 focus:outline-none rounded-lg px-6 py-2 text-white font-semibold shadow"
+    >
+      {{ $t("logs.refresh") }}
+    </button>
+
+    <div
+      class="px-4 py-3 mt-5 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800"
+    >
       <p class="text-xl mb-4">
         Rally {{ $t("logs.network") }} {{ $t("sidebar.logs") }}
       </p>
@@ -13,7 +22,7 @@
           style="left: 7px"
         ></div>
         <ul class="list-none m-0 p-0">
-          <template v-if="logs">
+          <template v-if="logs.length">
             <li class="mb-2" v-for="log in logs" :key="log.id">
               <div class="flex items-center mb-1">
                 <div
@@ -38,11 +47,54 @@
               </div>
             </li>
           </template>
+          <template v-else>
+            <div class="balls">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </template>
         </ul>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.balls {
+  width: 3.5em;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.balls div {
+  width: 0.8em;
+  height: 0.8em;
+  border-radius: 50%;
+  background-color: #fc2f70;
+  transform: translateY(-100%);
+  animation: wave 0.8s ease-in-out alternate infinite;
+}
+
+.balls div:nth-of-type(1) {
+  animation-delay: -0.4s;
+}
+
+.balls div:nth-of-type(2) {
+  animation-delay: -0.2s;
+}
+
+@keyframes wave {
+  from {
+    transform: translateY(-100%);
+  }
+  to {
+    transform: translateY(100%);
+  }
+}
+</style>
 
 <script>
 import Breadcrumbs from "@/components/Breadcrumbs";
@@ -62,18 +114,22 @@ export default {
       logs: [],
     };
   },
+  methods: {
+    refresh() {
+      fetch(
+        `https://api.rally.io/v1/creator_coins/${
+          this.coins[this.currentCoin].coinSymbol
+        }/network_activity`
+      )
+        .then((res) => res.json())
+        .then((response) => {
+          this.logs = response;
+        })
+        .catch(console.error);
+    },
+  },
   mounted() {
-    //TODO cache component
-    fetch(
-      `https://api.rally.io/v1/creator_coins/${
-        this.coins[this.currentCoin].coinSymbol
-      }/network_activity`
-    )
-      .then((res) => res.json())
-      .then((response) => {
-        this.logs = response;
-      })
-      .catch(console.error);
+    this.refresh();
   },
 };
 </script>
