66461e1266e77121527dffa087de091533c22df7
Ju99ernaut <brendonzee@gmail.com>
2020-12-21 17:02:43 +0200

    Update modal component

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,7 +2,6 @@
   <div
     :class="show ? '' : 'opacity-0 pointer-events-none'"
     class="modal fixed w-full h-full top-0 left-0 flex items-center justify-center"
-    @keyup.esc="show && closeModal()"
   >
     <div
       class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"
@@ -26,7 +25,6 @@
             d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
           ></path>
         </svg>
-        <span class="text-sm">(Esc)</span>
       </div>
       <slide-y-up-transition :duration="animationDuraion">
         <!-- component -->
