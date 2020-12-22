7ab370a6426d650fba6378d97bf53e398a17b8f4
Ju99ernaut <brendonzee@gmail.com>
2020-12-22 14:50:47 +0200

    Util for fetching crypto market data

---
+++ src/utils/coinData.js
@@ -0,0 +1,12 @@
+import fetch from './fetch';
+import config from '@/config';
+import queryString from './queryString';
+
+export default (endpoint, params = {}, clb = () => {}) => {
+    fetch(`${config.coingeckoApi}/${endpoint}${queryString(params)}`)
+        .then((res) => res.json())
+        .then((response) => {
+            clb(response);
+        })
+        .catch(console.error);
+};
