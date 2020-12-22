af7acee2bf98c6179e7a0c48735af641332a8222
Ju99ernaut <brendonzee@gmail.com>
2020-12-22 10:47:29 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,13 @@
 export default {
-    home: "/",
-    dashboard: "/dashboard/home",
-    clientId: "",
-    botUrl: "",
+    home: "http://localhost:8080/",
+    dashboard: "http://localhost:8080/dashboard/home/",
+    clientId: "786246670530773023",
+    botUrl: "https://discord.com/api/oauth2/authorize?client_id=769334375023640578&permissions=268438560&scope=bot",
+    rallyApi: "https://api.rally.io/v1",
+    coingeckoApi: "https://api.coingecko.com/api/v3",
+    discordApi: "https://discord.com/api",
+    discordCdn: "https://cdn.discordapp.com",
+    githubHome: "https://github.com/Ju99ernaut/rally-discordbot-dashboard",
     colors: {
         default: "#344675",
         primary: "#42b883",
