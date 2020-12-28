0e1f49833a395eaf82ce8b678263c27e95649dce
Ju99ernaut <brendonzee@gmail.com>
2020-12-29 00:43:32 +0200

    Update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     pwa: {
+        name: 'rallybot-dashboard',
+        themeColor: '#121317',
+        msTileColor: '#121317',
+        appleMobileAppCapable: 'yes',
+        appleMobileAppStatusBarStyle: 'black-translucent',
+        iconPaths: {
+            appleTouchIcon: 'img/icons/apple-icon-152x152.png',
+            msTileImage: 'img/icons/ms-icon-144x144.png'
+        },
         workboxPluginMode: 'InjectManifest',
         workboxOptions: {
             swSrc: 'public/service-worker.js'
